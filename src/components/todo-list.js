0c51ee55f9a74ebe1afdc5f9b5cff895df905245
Taras <tsolokha@gmail.com>
2020-05-09 00:13:47 +0300

    add bootstrap css

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,17 +1,18 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
+import "./todo-list.css";
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>
-                <TodoListItem />
+const TodoList = ({ todos }) => {
+    const elements = todos.map((el) => {
+        const { id, ...elProps } = el;
+        return (
+            <li key={id} className="list-group-item">
+                <TodoListItem {...elProps} />
             </li>
-            <li>
-                <TodoListItem />
-            </li>
-        </ul>
-    );
+        );
+    });
+
+    return <ul className="list-group todo-list">{elements}</ul>;
 };
 
 export default TodoList;
